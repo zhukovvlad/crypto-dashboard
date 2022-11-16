b50fc26a47e27560e561385a72bb39cc7ce92676
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-16 18:18:33 +0300

    Added colorMode Context into the project and App Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider theme={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
