c8d5e89cd95aa556b798263888444f45eb98e01b
Vladimir Zhukov <zhukovvlad@yandex.ru>
2022-11-18 19:10:34 +0300

    Delete firebase.config.js
    Deleted config files for Firebase from repository

--- src/firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyBC8YoDPYJ7efLMMU13gMQ76Se7iGgKQ5E",
-  authDomain: "crypto-dashboard-c1cd3.firebaseapp.com",
-  projectId: "crypto-dashboard-c1cd3",
-  storageBucket: "crypto-dashboard-c1cd3.appspot.com",
-  messagingSenderId: "979730257261",
-  appId: "1:979730257261:web:de72f1807bfb0c57557664",
-  measurementId: "G-XT3DKJ20LR",
-};
