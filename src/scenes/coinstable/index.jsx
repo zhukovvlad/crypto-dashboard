7cb61f839b8710b50ee6bdc2fc7088ddb42f273f
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-20 21:46:11 +0300

    Created Header Component

--- src/scenes/coinstable/index.jsx
+++ src/scenes/coinstable/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 function CoinsTable() {
-  return <div>CoinsTable</div>;
+  return (
+    <Box m="20px">
+      <Header title="Coin Table" subtitle="Table with all user's coins" />
+    </Box>
+  );
 }
 
 export default CoinsTable;
