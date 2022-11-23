a631abcbd6791784f1362c671243a9ca233b8cad
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-23 12:11:23 +0300

    Implemented CoinstDataTable component logic for correct fetching data from CoinGecko

--- src/components/CoinsDataTable.jsx
+++ src/components/CoinsDataTable.jsx
@@ -1,32 +1,51 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { CoinsData } from "../utils/apis";
-import { useDataApi } from "../hooks/useDataApi";
 
 const CoinsDataTable = ({ dataArray }) => {
-  const queryString = dataArray
-    .map((coin) => {
-      return coin.id;
-    })
-    .join();
 
-  const initialUrl = CoinsData(queryString);
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  console.log(initialUrl);
+  useEffect(() => {
+    const queryString = dataArray
+      .map((coin) => {
+        return coin.id;
+      })
+      .join();
 
-  const [{ data, isLoading, isError }, setUrl] = useDataApi(
-    initialUrl,
-    []
-  );
+    const initialUrl = CoinsData(queryString);
 
-  console.log("Data Array is ", data);
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios.get(CoinsData(initialUrl));
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [dataArray]);
 
   return (
     <div>
+      {isError && <div>We've got an error...</div>}
       {isLoading ? (
         <div>Loading table</div>
       ) : (
         <div>
-          {data.map((row) => (
-            <div key={row.id}>{row.name} - {row.symbol}</div>
+          {data?.map((row) => (
+            <div key={row.id}>
+              {row.name} - {row.symbol}
+            </div>
           ))}
         </div>
       )}
